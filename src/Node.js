1499ed9cfe065f18d53aa95d66ab18c578259b19
Kyle Simpson <kyleandrew.simpson@gmail.com>
2016-01-08 15:03:00 +0000

    Fix to a bad array access, causing crash.

--- src/Node.js
+++ src/Node.js
@@ -26,7 +26,7 @@ class Node{
 		//Improvement over std chord.
 		do {
 			this.finger[index++].node = node;
-		} while(ID.inRightOpenBound(node.id, this.finger[index.start], this.finger[index].node.id));
+		} while(ID.inRightOpenBound(node.id, this.finger[index].start, this.finger[index].node.id));
 	}
 
 	removeFinger(id){
